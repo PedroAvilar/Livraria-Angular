6d8e97ca34eedc5f71f6cb6de28eeb6c5dabc066
Pedro Avilar <pedrowa.avilar@gmail.com>
2024-05-28 20:20:44 -0300

    C04 - Atributo e métodos para a classe ControleEditoraService.

--- src/app/controle-editora.service.ts
+++ src/app/controle-editora.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Editora } from './editora';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleEditoraService {
-
+  //Atributo editoras com elementos no formato JSON.
+  private editoras: Array<Editora> = [
+    {codEditora: 123, nome: "Editora Janeiro"},
+    {codEditora: 456, nome: "Editora Fevereiro"},
+    {codEditora: 789, nome: "Editora Março"}
+  ]
+  //Método que retorna o nome da editora pelo código.
+  getNomeEditora(codEditora:Number): string {
+    const editora = this.editoras.filter(editora => editora.codEditora === codEditora)[0];
+    return editora.nome;
+  }
+  //Método que retorna o vetor das editoras.
+  getEditoras() {
+    return this.editoras;
+  }
   constructor() { }
 }
