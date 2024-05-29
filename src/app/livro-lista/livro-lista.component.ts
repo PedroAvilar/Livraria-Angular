94b33a00459ec3bb3841180699c12e2da29aa678
Pedro Avilar <pedrowa.avilar@gmail.com>
2024-05-29 16:12:01 -0300

    C08 - Atributos e métodos para a classe LivroListaComponent.

--- src/app/livro-lista/livro-lista.component.ts
+++ src/app/livro-lista/livro-lista.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
 
 @Component({
   selector: 'app-livro-lista',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './livro-lista.component.css'
 })
 export class LivroListaComponent {
+  //Atributo editoras que inicializa um vetor vazio.
+  public editoras: Array<Editora> = [];
+  //Atributo livros que inicializa um vetor vazio.
+  public livros: Array<Livro> = [];
 
+  //Serviços injetados através do construtor.
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService
+  ) {}
+  //Método que preenche os vetores editoras e livros.
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+    this.livros = this.servLivros.obterLivros();
+  }
+  //Método excluir que chama servLivros.
+  excluir = (codigo: number): void => {
+    this.servLivros.excluir(codigo);
+    this.livros = this.servLivros.obterLivros();
+  }
+  //Método obterNome que chama servEditora.
+  obterNome = (codEditora: Number): string => {
+    return this.servEditora.getNomeEditora(codEditora);
+  }
 }
