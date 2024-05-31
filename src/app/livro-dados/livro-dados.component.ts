867c8b42a4ecb1259f6a1393add88f3c023da4b8
Pedro Avilar <pedrowa.avilar@gmail.com>
2024-05-31 12:11:03 -0300

    C14 - Atributos e métodos para a classe LivroDadosComponent.

--- src/app/livro-dados/livro-dados.component.ts
+++ src/app/livro-dados/livro-dados.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Livro } from '../livro';
+import { Editora } from '../editora';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-livro-dados',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './livro-dados.component.css'
 })
 export class LivroDadosComponent {
+  //Atributo livro instanciado via construtor.
+  public livro: Livro;
+  //Atributo autoresForm que inicializa vazio.
+  public autoresForm:string = "";
+  //Atributo editoras que inicializa um vetor vazio.
+  public editoras: Array<Editora> = [];
 
+  //Serviços injetados através do construtor.
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router
+  ) {
+    this.livro = new Livro(0, 0, "", "", []);
+  }
+  //Método que preenche o vetor de editoras.
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+  /*Método incluir que preenche o aributo autores e
+  invoca incluir de controle-livros.service.*/
+  incluir = (): void => {
+    this.livro.autores = this.autoresForm.split("\n");
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl("/lista");
+  }
 }
